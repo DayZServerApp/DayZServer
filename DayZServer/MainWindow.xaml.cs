f7ad874336c4463e48e2047a3f57bc58c770b5cf
mitterw <ray.w.mitten@conocophillips.com>
2015-03-22 01:35:16 -0500

    Updated UI and Fixed Bugs
    Added Scrollview to server list added clear server button, fixed some
    other bugs in the flow

--- DayZServer/MainWindow.xaml.cs
+++ DayZServer/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace DayZServer
     {
 
         private static Timer aTimer;
-        
+
         public MainWindow()
         {
             InitializeComponent();
@@ -35,13 +35,8 @@ namespace DayZServer
             // Hook up the Elapsed event for the timer. 
             aTimer.Elapsed += OnTimedEvent;
             aTimer.Enabled = true;
-
-
-        }
-
-        void OnImageButtonClick(object sender, RoutedEventArgs e)
-        {
-           
+            clear_button.Visibility = Visibility.Hidden;
+   
         }
 
         void OnTimedEvent(Object source, ElapsedEventArgs e)
@@ -57,54 +52,54 @@ namespace DayZServer
                 string json = System.IO.File.ReadAllText(configpath);
                 string servername = json.Split(new string[] { "lastMPServerName=\"" }, StringSplitOptions.None)[1].Split(new string[] { "\";" }, StringSplitOptions.None)[0].Trim();
                 Console.WriteLine("Server Name" + servername);
+                string IPAddress = json.Split(new string[] { "lastMPServer=\""}, StringSplitOptions.None)[1].Split(new string[] { "\";" }, StringSplitOptions.None)[0].Trim();
+                Console.WriteLine("IP Address" + IPAddress);
                 string version = json.Split(new string[] { "version=" }, StringSplitOptions.None)[1].Split(new string[] { ";" }, StringSplitOptions.None)[0].Trim();
-                Console.WriteLine("Server Name" + version);
+                Console.WriteLine("Version" + version);
                 string appDataPath = Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData);
                 string path = System.IO.Path.Combine(appDataPath, "DayZServer");
+                
                 if (!Directory.Exists(path))
                 {
                     Directory.CreateDirectory(path);
                 }
-                   
+
+
                 this.Dispatcher.Invoke((Action)(() =>
                 {
 
-                    
-                    
                     string serverhistorypath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), path, "dayzhistory.txt");
                     string currentserverpath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), path, "dayzserver.txt");
                     string currentserver = readCurrentServer(currentserverpath);
-                    //readServerHistoryList(serverhistorypath);
-                    //string previousserver = serverTextBox.Text;
-
                     string currentserverNormalized = Regex.Replace(currentserver, @"\s+", String.Empty);
-                    string servernameNormalized = Regex.Replace(servername, @"\s+", String.Empty);
+                    string servernameNormalized = Regex.Replace(servername + "\r\nIP Address: " + IPAddress, @"\s+", String.Empty);
 
                     if (currentserverNormalized != servernameNormalized)
                     {
-                        if (currentserver != "No Server History Available") 
-                        { 
-                        writeServerHistoryList(serverhistorypath, currentserver);
+                        if (currentserver != "No Server History Available")
+                        {
+                            writeServerHistoryList(serverhistorypath, currentserver);
                         }
                         else
                         {
                             serverList.Text = "No Server History Available";
+                            clear_button.Visibility = Visibility.Hidden;
                         }
+
+                        writeCurrentServer(currentserverpath, servername + "\r\nIP Address: " + IPAddress);
                         
-                        writeCurrentServer(currentserverpath, servername);
-                        //serverTextBox.Text = servername;
                     }
                     else
                     {
                         //writeCurrentServer(currentserverpath, servername);
                         serverTextBox.Text = currentserver;
                         readServerHistoryList(serverhistorypath);
-                        
+
                     }
-                    
+
                     versionlabel.Content = version;
                     DoubleAnimation animation = new DoubleAnimation(0, TimeSpan.FromSeconds(2));
-                    c.BeginAnimation(Canvas.OpacityProperty, animation); 
+                    c.BeginAnimation(Canvas.OpacityProperty, animation);
 
                 }));
 
@@ -117,19 +112,18 @@ namespace DayZServer
         }
 
 
-
-
         public void writeServerHistoryList(string serverhistorypath, string server)
         {
 
-            string temphistory;
-            string lastLine = File.ReadLines(serverhistorypath).Last();
+
             if (File.Exists(serverhistorypath))
             {
+                clear_button.Visibility = Visibility.Visible;
+                string temphistory;
 
                 try
                 {
-                    
+
                     using (StreamReader sreader = new StreamReader(serverhistorypath))
                     {
                         temphistory = sreader.ReadToEnd();
@@ -143,6 +137,8 @@ namespace DayZServer
                         sw.Write(temphistory);
                         sw.Close();
                         readServerHistoryList(serverhistorypath);
+                        
+                        
                     }
 
 
@@ -152,64 +148,67 @@ namespace DayZServer
 
                 }
             }
-            else 
-            { 
-            try
+            else
             {
-
-                using (StreamWriter sw = File.CreateText(serverhistorypath))
+                clear_button.Visibility = Visibility.Visible;
+                try
                 {
 
-                    if (sw.BaseStream != null)
+                    using (StreamWriter sw = File.CreateText(serverhistorypath))
                     {
-                        
-                        sw.BaseStream.Seek(0, SeekOrigin.End);
-                        sw.WriteLine(server);
-                        sw.Close();
-                        readServerHistoryList(serverhistorypath);
+
+                        if (sw.BaseStream != null)
+                        {
+
+                            sw.BaseStream.Seek(0, SeekOrigin.End);
+                            sw.WriteLine(server);
+                            sw.Close();
+                            readServerHistoryList(serverhistorypath);
+                            
+                        }
+
                     }
 
                 }
+                catch (Exception)
+                {
 
-            }
-            catch (Exception)
-            {
-
-            }
+                }
 
             }
         }
 
         public void readServerHistoryList(string serverhistorypath)
-       {
+        {
 
-           if (File.Exists(serverhistorypath))
-           {
-               try
-               {
-                   // Create an instance of StreamReader to read from a file. 
-                   // The using statement also closes the StreamReader. 
-                   using (StreamReader sw = new StreamReader(serverhistorypath))
-                   {
-                       String line = sw.ReadToEnd();
-                       serverList.Text = line;
-                   }
-               }
-               catch (Exception e)
-               {
-                   // Let the user know what went wrong.
-                   Console.WriteLine("The file could not be read:");
-                   Console.WriteLine(e.Message);
-               }
-           }
-           else
-           {
-               //writeServerHistoryList(serverhistorypath, "No Server History");
-           }
+            if (File.Exists(serverhistorypath))
+            {
+                try
+                {
+                    // Create an instance of StreamReader to read from a file. 
+                    // The using statement also closes the StreamReader. 
+                    using (StreamReader sw = new StreamReader(serverhistorypath))
+                    {
+                        String line = sw.ReadToEnd();
+                        serverList.Text = line;
+                    }
+                }
+                catch (Exception e)
+                {
+                    // Let the user know what went wrong.
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(e.Message);
+                }
+            }
+            else
+            {
+                //writeServerHistoryList(serverhistorypath, "No Server History");
+                serverList.Text = "No Server History Available";
+            }
 
-       }
+        }
 
-       String readCurrentServer(string currentserverpath)
+        String readCurrentServer(string currentserverpath)
         {
 
             if (File.Exists(currentserverpath))
@@ -239,35 +238,48 @@ namespace DayZServer
 
         }
 
-       public void writeCurrentServer(string currentserverpath, string server)
-       {
-           try
-           {
+        public void writeCurrentServer(string currentserverpath, string server)
+        {
+            try
+            {
 
-               using (StreamWriter sw = File.CreateText(currentserverpath))
-               {
+                using (StreamWriter sw = File.CreateText(currentserverpath))
+                {
 
-                   if (sw.BaseStream != null)
-                   {
-                       sw.WriteLine(server);
-                       sw.Close();
-                       readCurrentServer(currentserverpath);
-                   }
+                    if (sw.BaseStream != null)
+                    {
+                        sw.WriteLine(server);
+                        sw.Close();
+                        readCurrentServer(currentserverpath);
+                    }
 
-               }
+                }
 
-           }
-           catch (Exception)
-           {
+            }
+            catch (Exception)
+            {
 
-           }
-       }
+            }
+        }
+
+        private void ClearHistory(object sender, RoutedEventArgs e)
+        {
+            string appDataPath = Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData);
+            string path = System.IO.Path.Combine(appDataPath, "DayZServer");
+            string serverhistorypath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), path, "dayzhistory.txt");
+            if (File.Exists(serverhistorypath))
+            {
+                File.Delete(serverhistorypath);
+               
+            }
+
+            serverList.Text = "No Server History Available";
+            clear_button.Visibility = Visibility.Hidden;
+            
+        }
 
 
-   }
-
-
-
+    }
 
 
 
