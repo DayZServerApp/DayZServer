dcc58b1de1d13329505b0b47b66db6d466364561
VG7 <multimediax@hotmail.com>
2017-05-24 20:49:38 -0500

    added helixtoolkit

--- DayZServer/Map.xaml.cs
+++ DayZServer/Map.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelixToolkit.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Shapes;
 
 namespace DayZServer
@@ -19,9 +21,20 @@ namespace DayZServer
     /// </summary>
     public partial class Map : Window
     {
+
+
+        Model3DGroup map;
+
+        Model3D terrain;
         public Map()
         {
             InitializeComponent();
+            ModelImporter importer = new ModelImporter();
+
+            map = new Model3DGroup();
+
+            //load the files
+            terrain = importer.Load(@"Map/map.obj");
         }
     }
 }
